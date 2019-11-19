143d6de92008bfa25191f1b3bf8e929b6260b7a3
Sarah <sarahhargreaves10@gmail.com>
2019-11-19 15:27:41 -0500

    Initial commit

---
+++ src/main/java/com/rabbitmq/rabbitmqmessaging/Receiver.java
@@ -0,0 +1,20 @@
+package com.rabbitmq.rabbitmqmessaging;
+
+import java.util.concurrent.CountDownLatch;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    CountDownLatch getLatch() {
+        return latch;
+    }
+
+}
